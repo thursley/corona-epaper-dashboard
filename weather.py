5652db940232f30abaf955feb282133e6e04842a
Henry <henry.preusser@posteo.de>
2021-05-05 19:43:12 +0200

    add weather module

---
+++ weather.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import os
+
+api_key = os.environ.get('OWM_API_KEY')
+
+kelvin_offset = -273.15
+city = 'Jena'
+
+rsp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}')
+if not rsp.ok:
+    print(f'could not retrieve weather data for {city}. abort.')
+    exit(1)
+
+data = json.loads(rsp.content)
+
+def get_temperature():
+    temperature_c = round(data['main']['temp'] + kelvin_offset, 1)
+    return temperature_c
+
+if __name__ == '__main__':
+    print(f'{get_temperature()} °C')
+
